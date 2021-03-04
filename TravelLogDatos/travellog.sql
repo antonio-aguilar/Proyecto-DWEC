5142201b18b779e69d754c2a8cf1c8e8ef0194d1
Jesús David <46890277+jesusdavidguti@users.noreply.github.com>
2021-03-04 18:32:52 +0100

    Update travellog.sql

--- TravelLogDatos/travellog.sql
+++ TravelLogDatos/travellog.sql
@@ -20,6 +20,9 @@ SET time_zone = "+00:00";
 -- Base de datos: `travellog`
 --
 
+DROP TABLE `hitos`;
+DROP TABLE `itinerario`;
+DROP TABLE `viaje`;
 -- --------------------------------------------------------
 
 --
