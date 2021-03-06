5d43b89594d38d9e0eb65ae597e8cbdcb0f6b784
Antonio Aguilar <55695218+antonio-aguilar@users.noreply.github.com>
2021-03-06 14:32:12 +0100

    Archivos de la API

---
+++ src/main/java/com/travellog/apiTravellog/ApiTravellogApplication.java
@@ -0,0 +1,15 @@
+package com.travellog.apiTravellog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTravellogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTravellogApplication.class, args);
+		
+		System.out.println("Aplicación iniciada");
+	}
+
+}
---
+++ src/test/java/com/travellog/apiTravellog/ApiTravellogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travellog.apiTravellog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTravellogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
